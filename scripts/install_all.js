5f1889b5ea53e31ce0189793cfa977ebc1e0f0bc
Kajetan Owczarek <flash3ch@gmail.com>
2023-03-13 18:52:52 +0100

    deployment script

--- scripts/install_all.js
+++ scripts/install_all.js
@@ -1,12 +1,22 @@
+const fs = require('fs').promises
 const path = require('path')
 const simpleGit = require('simple-git')
 
+const installsFolder = 'branch_installs'
 
 async function Main () {
-    const containingGit = simpleGit.simpleGit(path.resolve(__dirname), '../')
+    const originPath = path.resolve(__dirname, '../') 
+    const containingGit = simpleGit.simpleGit(originPath)
     const branchesAvailable = await containingGit.branch()
     const deployableBranches = branchesAvailable.all.filter(name => name.startsWith('ver'))
-    console.log(deployableBranches)
+
+    for(const branch of deployableBranches) {
+        const localPath = path.resolve(__dirname, '..', installsFolder, branch)
+        await fs.mkdir(localPath, {recursive: true})
+        fs.cp(originPath, localPath, {filter: (source, dest) => !source.includes(installsFolder)})
+        const localGit = simpleGit.simpleGit(localPath)
+        await localGit.checkoutLocalBranch(branch)
+    }
 }
 
 Main()
