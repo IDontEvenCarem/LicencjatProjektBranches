e9ff1a0715833b0e2e5ea670068d36bdf4f19fdc
Kajetan Owczarek <flash3ch@gmail.com>
2023-03-13 19:03:48 +0100

    further install script update

--- scripts/install_all.js
+++ scripts/install_all.js
@@ -1,3 +1,4 @@
+const os = require('os')
 const fs = require('fs').promises
 const path = require('path')
 const simpleGit = require('simple-git')
@@ -10,13 +11,19 @@ async function Main () {
     const branchesAvailable = await containingGit.branch()
     const deployableBranches = branchesAvailable.all.filter(name => name.startsWith('ver'))
 
+    const tempPath = await fs.mkdtemp(path.join(os.tmpdir(), 'kolpb-'))
+    await fs.cp(originPath, tempPath, {recursive: true, filter: (source, dest) => !source.includes(installsFolder)})
+
     for(const branch of deployableBranches) {
+        console.log("Setting up", branch);
         const localPath = path.resolve(__dirname, '..', installsFolder, branch)
         await fs.mkdir(localPath, {recursive: true})
-        fs.cp(originPath, localPath, {filter: (source, dest) => !source.includes(installsFolder)})
+        await fs.cp(tempPath, localPath, {recursive: true})
         const localGit = simpleGit.simpleGit(localPath)
-        await localGit.checkoutLocalBranch(branch)
+        await localGit.checkout(branch);
     }
+
+    await fs.rmdir(tempPath, { recursive: true, force: true });
 }
 
 Main()
