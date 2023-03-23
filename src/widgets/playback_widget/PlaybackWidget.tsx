110e16b5f1c07f830aae6b9b11ca16492cff8e2d
Kajetan Owczarek <flash3ch@gmail.com>
2023-03-23 14:03:20 +0100

    Better autoplay behaviour

--- src/widgets/playback_widget/PlaybackWidget.tsx
+++ src/widgets/playback_widget/PlaybackWidget.tsx
@@ -1,11 +1,17 @@
 import './PlaybackWidget.css'
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export default function PlaybackWidget () {
     const [closed, setClosed] = useState(true)
+    const videoRef = useRef(null)
 
     useEffect(() => {
         const sideAd = document.querySelector('.side-add')
+
+        document.addEventListener('click', function clickDocument(){
+            document.removeEventListener('click', clickDocument);
+            (videoRef.current as any).play()
+        })
 
         if (sideAd) {
             setClosed(false)
@@ -22,7 +28,7 @@ export default function PlaybackWidget () {
                 <strong>Wiosna się zaczyna!</strong>
                 <span onClick={() => setClosed(true)} className={"material-symbols-rounded close-button"}>close</span>
             </div>
-            <video controls width="960" height="540" autoPlay loop>
+            <video ref={videoRef} controls width="960" height="540" autoPlay loop>
                 <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" type="video/webm"></source>
                 <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" type="video/mp4"></source>
                 Download the
