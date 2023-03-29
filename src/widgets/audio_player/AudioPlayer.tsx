e646f233888e0d29c17687d4654f86a1e41aaf94
Kajetan Owczarek <flash3ch@gmail.com>
2023-03-29 19:47:05 +0200

    Start of audio player element

---
+++ src/widgets/audio_player/AudioPlayer.tsx
@@ -0,0 +1,16 @@
+import { faker } from '@faker-js/faker'
+import './AudioPlayer.css'
+
+export default function AudioPlayer () {
+    return (
+        <div className="audio-player">
+            <div className='audio-player-background'>
+                <div className='audio-player-play-button'></div>
+                <div className='audio-player-center'>
+                    {faker.company.catchPhrase()}
+                </div>
+                <div className='audio-player-stats'></div>
+            </div>
+        </div>
+    )
+}
