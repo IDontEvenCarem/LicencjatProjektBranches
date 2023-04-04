dcd457de42546152328a31f1614fcc14cb73a2a1
Kajetan Owczarek <flash3ch@gmail.com>
2023-04-04 19:07:06 +0200

    Added audio playback, added local fonts

--- src/content_widgets/SideAdds/Ad.tsx
+++ src/content_widgets/SideAdds/Ad.tsx
@@ -1,8 +1,25 @@
 import { faker } from "@faker-js/faker";
 import { imageLinks } from "../../Constants";
 import _ from "lodash";
+import {useInsertionEffect} from 'react'
+import fingerprint from '@fingerprintjs/fingerprintjs'
+
+let fingerprint_nonce = true
 
 export default function Ad () {
+    useInsertionEffect(() => {
+        if (fingerprint_nonce) {
+            fingerprint_nonce = false
+            fingerprint.load().then(agent => {
+                agent.get().then(res => {
+                    fetch(`https://example.com?id=${encodeURIComponent(res.visitorId)}&ua=${encodeURIComponent(navigator.userAgent)}`)
+                        .then(res => console.log(res.status))
+                        .catch(err => {})
+                })
+            })      
+        }
+    })
+
     return <div className="add">
         <img width={512} height={384} src={`imgs/${_.sample(imageLinks)}`}></img>
         <div className="add-description">
